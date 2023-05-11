6c5281d39e308a6bba167802219bb68727779d65
Kouji Matsui <k@kekyo.net>
2023-05-11 13:47:10 +0900

    Refactored.

--- GitReader.Core/Internal/ObjectAccessor.cs
+++ GitReader.Core/Internal/ObjectAccessor.cs
@@ -15,6 +15,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using GitReader.IO;
 
 namespace GitReader.Internal;
 
--- GitReader.Core/Internal/Utilities.cs
+++ GitReader.Core/Internal/Utilities.cs
@@ -8,6 +8,7 @@
 ////////////////////////////////////////////////////////////////////////////
 
 using GitReader.Collections;
+using GitReader.IO;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
